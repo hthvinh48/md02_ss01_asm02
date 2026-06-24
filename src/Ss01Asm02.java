a528a2c86989eded87bcc504594b52cee912c381
Hiển Vinh <hthvinh48@gmail.com>
2026-06-24 17:32:02 +0700

    handle exceptions

---
+++ src/Ss01Asm02.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Ss01Asm02 {
+    public static void main(String[] args) {
+        int firstNumber = 0;
+        int secondNumber = 0;
+
+        try (Scanner input = new Scanner(System.in)) {
+            while (true) {
+                try {
+                    System.out.print("Nhập số thứ nhất (firstNumber): ");
+                    firstNumber = Integer.parseInt(input.nextLine());
+                    System.out.print("Nhập số thứ hai (secondNumber): ");
+                    secondNumber = Integer.parseInt(input.nextLine());
+
+                    if (secondNumber == 0) {
+                        System.out.println("Không thể thực hiện phép chia vì giá trị thứ 2 bằng 0!");
+                        continue;
+                    }
+
+                    int sum = firstNumber + secondNumber;
+                    int subtraction = firstNumber - secondNumber;
+                    int multiplication = firstNumber * secondNumber;
+                    int division = firstNumber / secondNumber;
+                    int remainder = firstNumber % secondNumber;
+
+                    System.out.println();
+                    System.out.println("--- Kết quả ---");
+                    System.out.println("firstNumber: " + firstNumber);
+                    System.out.println("secondNumber: " + secondNumber);
+                    System.out.println("Tổng: " + sum);
+                    System.out.println("Hiệu: " + subtraction);
+                    System.out.println("Tích: " + multiplication);
+                    System.out.println("Thương: " + division);
+                    System.out.println("Phần dư: " + remainder);
+
+                    break;
+
+                } catch (NumberFormatException e) {
+                    System.out.println("Lỗi: Giá trị nhập vào phải là số!");
+                }
+            }
+        }
+    }
+}
